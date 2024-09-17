01caaffb2a2d26581493987520132c376bf462e9
Damir <absalyamov05@list.ru>
2024-09-17 05:29:52 +0300

    Задание 2 Пункт 1

--- Lab2LOAIZ/Lab2LOAIZ/Lab2LOAIZ.cpp
+++ Lab2LOAIZ/Lab2LOAIZ/Lab2LOAIZ.cpp
@@ -5,130 +5,78 @@
 
 using namespace std;
 
-int main(void)
+void shell(int* items, int count)
+{
+
+	int i, j, gap, k;
+	int x, a[5];
+
+	a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+
+	for (k = 0; k < 5; k++) {
+		gap = a[k];
+		for (i = gap; i < count; ++i) {
+			x = items[i];
+			for (j = i - gap; (x < items[j]) && (j >= 0); j = j - gap)
+				items[j + gap] = items[j];
+			items[j + gap] = x;
+		}
+	}
+}
+
+void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
+{
+	int i, j;
+	int x, y;
+
+
+
+	i = left; j = right;
+
+	/* выбор компаранда */
+	x = items[(left + right) / 2];
+
+	do {
+		while ((items[i] < x) && (i < right)) i++;
+		while ((x < items[j]) && (j > left)) j--;
+
+		if (i <= j) {
+			y = items[i];
+			items[i] = items[j];
+			items[j] = y;
+			i++; j--;
+		}
+	} while (i <= j);
+
+	if (left < j) qs(items, left, j);
+	if (i < right) qs(items, i, right);
+}
+
+
+int main()
 {
 	setlocale(LC_ALL, "ru");
-	setvbuf(stdin, NULL, _IONBF, 0);
-	setvbuf(stdout, NULL, _IONBF, 0);
+	srand(time(NULL));
 
-	clock_t start, end; // объявляем переменные для определения времени выполнения
+	clock_t start, end;
 
-	cout << "Выберите размер матриц:\n1 - 100\n2 - 200\n3 - 400\n4 - 1000\n5 - 2000\n6 - 4000\n7 - 10000\nВыбор: ";
-	int nabor;
-	cin >> nabor;
-	int razm;
-	switch (nabor)
+	const int SIZE = 10000;
+	int mass[SIZE];
+
+	for (int i = 0; i < SIZE; i++)
 	{
-	case 1:
-		razm = 100;
-		break;
-	case 2:
-		razm = 200;
-		break;
-	case 3:
-		razm = 400;
-		break;
-	case 4:
-		razm = 1000;
-		break;
-	case 5:
-		razm = 2000;
-		break;
-	case 6:
-		razm = 4000;
-		break;
-	case 7:
-		razm = 10000;
-		break;
+		mass[i] = rand() % 100 + 1;
 	}
-
-	int** ptr_razm_a = new int* [razm];
-
-	for (int i = 0; i < razm; i++)
-	{
-		ptr_razm_a[i] = new int[razm];
-	}
-
-	int** ptr_razm_b = new int* [razm];
-
-	for (int i = 0; i < razm; i++)
-	{
-		ptr_razm_b[i] = new int[razm];
-	}
-
-	int** ptr_razm_c = new int* [razm];
-
-	for (int i = 0; i < razm; i++)
-	{
-		ptr_razm_c[i] = new int[razm];
-	}
-
-	int i = 0, j = 0, r;
-	//int a[200][200], b[200][200], c[200][200], elem_c;
-
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	while (i < razm)
-	{
-		while (j < razm)
-		{
-			ptr_razm_a[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
-			j++;
-		}
-		i++;
-	}
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	i = 0; j = 0;
-	while (i < razm)
-	{
-		while (j < razm)
-		{
-			ptr_razm_b[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
-			j++;
-		}
-		i++;
-	}
-	int elem_c;
 
 	start = clock();
-
-	for (i = 0; i < razm; i++)
-	{
-		for (j = 0; j < razm; j++)
-		{
-			elem_c = 0;
-			for (r = 0; r < razm; r++)
-			{
-				elem_c = elem_c + ptr_razm_a[i][r] * ptr_razm_b[r][j];
-				ptr_razm_c[i][j] = elem_c;
-			}
-		}
-	}
-
+	//qs(mass, 0, SIZE-1);
+	//shell(mass, SIZE);
 	end = clock();
 
 	double vremya;
 	vremya = ((double)start / (double)end) / CLOCKS_PER_SEC;
-	cout << "Время работы программы: " << vremya;
 
-	for (int i = 0; i < razm; i++)
-	{
-		delete[]ptr_razm_c[i];
-	}
-	delete[]ptr_razm_c;
+	cout << "\nВремя выполнения программы: " << vremya;
 
-	for (int i = 0; i < razm; i++)
-	{
-		delete[]ptr_razm_b[i];
-	}
-	delete[]ptr_razm_b;
-
-	for (int i = 0; i < razm; i++)
-	{
-		delete[]ptr_razm_a[i];
-	}
-	delete[]ptr_razm_a;
-
-	return(0);
+	return 0;
 }
-
-
